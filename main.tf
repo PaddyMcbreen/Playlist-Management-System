e5de5654b29e7fe57c37252d4d5dea49c0479162
PaddyMcbreen <patrickmcbreen2004@hotmail.com>
2023-12-09 18:11:27 +0000

    Further instructions created for data block - more tracks added to the resource block + string changes

--- main.tf
+++ main.tf
@@ -1,11 +1,21 @@
+/*
+You can also add:
+
+name = (input song)
+album = (input album)
+
+Into the data block below to further specify the tracks you want in yor playlist
+*/
+
 data "spotify_search_track" "by_artist" {
   artist = "Travis Scott"
-
 }
 
+
+
 resource "spotify_playlist" "my-playlist" {
-  name        = "Paddys Playlist"
-  description = "This playlist was created by Terraform"
+  name        = "The Best of Travis Scott"
+  description = "This playlist was created by Terraform - Current Top 10 Travis Scott Songs"
   public      = true
 
   tracks = [
@@ -14,5 +24,11 @@ resource "spotify_playlist" "my-playlist" {
     data.spotify_search_track.by_artist.tracks[2].id,
     data.spotify_search_track.by_artist.tracks[3].id,
     data.spotify_search_track.by_artist.tracks[4].id,
+
+    data.spotify_search_track.by_artist.tracks[5].id,
+    data.spotify_search_track.by_artist.tracks[6].id,
+    data.spotify_search_track.by_artist.tracks[7].id,
+    data.spotify_search_track.by_artist.tracks[8].id,
+    data.spotify_search_track.by_artist.tracks[9].id,
   ]
 }
